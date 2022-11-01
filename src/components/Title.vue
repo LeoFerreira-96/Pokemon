25ad0e00d3a166095c8462e6993b1f8bb2f56739
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2022-11-01 00:17:50 -0300

    component Title

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+    <img class="title" src="../../assets/Pokémon-Logo.png" />
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<style scoped>
+.title {
+  left:50%;
+  width: 230px;
+  padding:2vmin;
+  position:absolute;
+  margin-top: 40px;
+  transform:translate(-50%, -50%);
+}
+
+</style>
