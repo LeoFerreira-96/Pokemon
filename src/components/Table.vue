42dfeba9a96d328af88bf366a0dd2b1e67bbc655
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2022-11-01 00:18:45 -0300

    component Table

---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <DataTable :value="pokemon">
+        <Column field="id" header="#"></Column>
+        <Column field="name" header="Nome"></Column>
+        <Column field="height" header="Altura"></Column>
+        <Column field="weight" header="Peso"></Column>
+        <Column field="buttons" header="Botões"></Column>
+        </DataTable>
+    </div>
+</template>
+
+<script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+export default {
+  name: 'Table',
+  components:{
+    DataTable,
+    Column
+  }
+}
+</script>
+
+<style scoped>
+.container{
+    border: 1px solid #000000;
+    border-radius: 10px;
+    padding: 15px;
+    width: 65%;
+}
+</style>
