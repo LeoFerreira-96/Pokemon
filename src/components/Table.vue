7709d248534ef38a044000414788523a70e4fa7b
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2022-11-04 02:18:25 -0300

    importando components,criando layout e function

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,55 +1,92 @@
 <template>
     <div class="container">
-        <DataTable :value="pokemons" responsiveLayout="scroll" :paginator="true" :rows="25">
-        <Column field="id" header="#"></Column>
-        <Column field="name" header="Nome"></Column>
-        <Column field="height" header="Altura"></Column>
-        <Column field="weight" header="Peso"></Column>
-        <Column field="buttons" header="Botões"></Column>
+        <DataTable :value="this.pokemons" :paginator="true" :rows="6" v-model:selection="selectedProduct1" selectionMode="single" dataKey="id" @rowSelect="ClickShow($event)">
+          <Column field="id" header="#" :sortable="true"></Column>
+          <Column field="name" header="Nome" :sortable="true"></Column>
+          <Column field="height" header="Altura" :sortable="true"></Column>
+          <Column field="weight" header="Peso" :sortable="true"></Column>
         </DataTable>
+        <Dialog v-model:visible="display" :style="{width: '35vw'}">
+          <template #header>
+            <h3>{{this.infoPokemon.name}}</h3>
+          </template>
+            <div class="content">
+              <Image :src="this.infoPokemon.image" :alt="this.pokemons.name" width="150" />
+            </div>
+            <h3>Habilidades</h3>
+            <ul id="abilities">
+              <li v-for="item in this.infoPokemon.abilities" :key="item">
+                {{ item }}
+              </li>
+            </ul>
+
+          <template #footer>
+              <Button label="Fechar" autofocus @click="ClickClose()"/>
+        </template>
+      </Dialog>
     </div>
 </template>
 
 <script>
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
+
 import api from '../services/api'
 export default {
   name: 'Table',
-  components:{
-    DataTable,
-    Column
-  },
+
   data() {
-        return {
-            pokemons: []
-        }
-    },
-    mounted() {
-        this.getPokemons()
-    },
-    methods: {
-      async getPokemons() {
-        try {
-          const pokemons = await api.get('pokemon?limit=151&offset=0')
-        this.pokemons = pokemons.data.results.map(async ( _,index) => {
-          const id = Number(index+1)
-          return this.pokemons = await api.get(`pokemon/${id}`)
+    return {
+      infoPokemon:'',
+      pokemons:[],
+      display:false
+    }
+  },
+  mounted() {
+    this.getPokemons()
+  },
+  methods: {
+    async getPokemons() {
+      const { data: {results}} = await api.get('pokemon?limit=151&offset=0');
+      for await(let pokemon of results){
+        const { data } = await api.get(`pokemon/${pokemon.name}`);
+        this.pokemons.push({
+          "id":data.id,
+          "name": data.name,
+          "height": `${(data.height*10)} cm`,
+          "weight": `${(data.weight/10)} kg`,
+          "abilities": data.abilities.map((e)=>{
+            return e.ability.name
+          }),
+          "image": data.sprites.front_default
+
         })
-        }catch (error) {
-          console.log(error)
-        }
       }
+    },
+
+    ClickShow(event) {
+      this.display=true
+      this.infoPokemon= event.data
+      console.log(event.data)
+    },
+    ClickClose() {
+      this.display=false
     }
   }
+}
 
 </script>
 
 <style scoped>
 .container{
     border: 1px solid #000000;
-    border-radius: 10px;
-    padding: 15px;
-    width: 65%;
+    width: 100%;
+}
+
+.dialog {
+  max-width: 300px;
+}
+.content{
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
+
+import Dialog from 'primevue/dialog';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+import Image from 'primevue/image';
+
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
 
 const app = createApp(App);
 
 app.use(PrimeVue);
+
+app.component('Dialog', Dialog);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('Button', Button);
+app.component('Image', Image);
+
 app.mount("#app");
